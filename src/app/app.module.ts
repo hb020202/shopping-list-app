3c2faf6230a47d8286e28838e367de3514b16dba
Holly Bedford <holly.bedford@manniondaniels.com>
2022-08-07 15:22:37 +0100

    add-item component and list-data service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListViewComponent } from './list-view/list-view.component';
+import { AddItemComponent } from './add-item/add-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListViewComponent
+    ListViewComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
