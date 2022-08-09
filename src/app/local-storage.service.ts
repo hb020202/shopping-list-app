c44de04aa66ada893b227a59a9cd546720df411f
Holly Bedford <holly.bedford@manniondaniels.com>
2022-08-09 13:07:34 +0100

    added local storage service

---
+++ src/app/local-storage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  localStorage;
+  listItems: any[] = [];
+
+  constructor() {
+    this.localStorage = window.localStorage;
+  }
+
+  addListItem(item: string): void {
+    this.listItems.splice(0, 0, {item});
+    this.setLocalStorage(this.listItems);
+    this.getLocalStorage();
+  }
+
+  setLocalStorage(list): void {
+    console.log(this.localStorage = JSON.stringify(list));
+    this.localStorage = JSON.stringify(list);
+  }
+
+  getLocalStorage(): string[] {
+    return this.localStorage.listItems;
+  }
+}
