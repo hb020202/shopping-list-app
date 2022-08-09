3acefeefc6eb03176058f5b2ffb413bce3a850f9
Holly Bedford <holly.bedford@manniondaniels.com>
2022-08-09 16:49:25 +0100

    removed unused test

--- src/app/list-view/list-view.component.spec.ts
+++ src/app/list-view/list-view.component.spec.ts
@@ -22,9 +22,4 @@ describe('ListViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should remove item from list', () => {
-    component.listItems = [{item: 'Apple'}, {item: 'Orange'}];
-    component.removeItem(0);
-    expect(component.listItems).toEqual([{item: 'Orange'}]);
-  });
 });
