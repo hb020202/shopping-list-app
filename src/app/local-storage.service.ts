bcf7f614ad75145cbad63ead2ae1be52f8f139ab
Holly Bedford <holly.bedford@manniondaniels.com>
2022-08-09 16:25:36 +0100

    changed stringify/parse again, changed getLocalStorage to use getter

--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -10,19 +10,19 @@ export class LocalStorageService {
 
   constructor() {
     this.localStorage = window.localStorage;
+    this.listItems = this.getLocalStorage();
   }
 
   addListItem(item: string): void {
     this.listItems.splice(0, 0, {item});
     this.setLocalStorage(this.listItems);
-    this.getLocalStorage();
   }
 
   setLocalStorage(list): void {
-    this.localStorage.setItem('list', list);
+    this.localStorage.setItem('list', JSON.stringify(list));
   }
 
-  getLocalStorage(): Storage {
-    return JSON.parse(this.localStorage.list);
+  getLocalStorage(): string[] {
+    return JSON.parse(this.localStorage.getItem('list'));
   }
 }
